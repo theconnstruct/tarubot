7abab134259c2e7fedefd8cdbb5aa79d73fbe401
Connor Maddox <connor@connstruct.tech>
2025-02-06 15:47:01 -0600

    Fail if API token not provided.

--- tarubot/main.py
+++ tarubot/main.py
@@ -1,8 +1,13 @@
 from disnake.ext import commands
+import logging
 import os
 
 
 def main():
+    if not os.environ.get("DISCORD_API_TOKEN"):
+        logging.critical("No API token provided. Set DISCORD_API_TOKEN.")
+        exit(1)
+
     test_guild_ids = (
         list(map(int, os.environ.get("DISCORD_GUILD_IDS").split(",")))
         if os.environ.get("DISCORD_GUILD_IDS")
