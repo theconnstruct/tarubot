4b958aced54bbb5581f995dbf7dc78e0e83391c1
Connor Maddox <connor@connstruct.tech>
2025-02-07 09:02:56 -0600

    Test Nodestone connection.

--- tarubot/cogs/utility.py
+++ tarubot/cogs/utility.py
@@ -1,4 +1,7 @@
+import json
+
 import disnake
+import requests
 from disnake.ext import commands
 
 
@@ -10,10 +13,18 @@ class UtilityCommandsCog(commands.Cog):
     @commands.slash_command(description="Check bot alive status and latency.")
     async def ping(self, interaction: disnake.ApplicationCommandInteraction):
         await interaction.send(
-            "Pong! Current websocket latency is {} milliseconds.".format(
-                int(self.bot.latency * 1000)
-            ),
-            ephemeral=True,
+                "Pong! Current websocket latency is {} milliseconds.".format(
+                        int(self.bot.latency * 1000)
+                ),
+                ephemeral=True,
+        )
+
+    @commands.slash_command(description="Test nodestone worker connectivity.")
+    async def test(self, interaction: disnake.ApplicationCommandInteraction):
+        s = requests.session()
+        r = s.get('http://nodestone:8080/Character/38371223')
+        await interaction.send("```json\n{}\n```".format(
+                json.dumps(r.json(), indent=2))
         )
 
 
